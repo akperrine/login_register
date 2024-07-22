27fb14c1140dd8a8baebc8ab7425e4910c241728
Austin Perrine <akperrine@gmail.com>
2024-07-22 16:15:12 -0700

    have pg retrieve and print user upon getUser

--- main.cpp
+++ main.cpp
@@ -4,21 +4,66 @@
 #include <string>
 using std::string;
 
-void insertUser(PGconn *conn) {
-     string username;
+struct Credentials {
+    string username;
+    string password;
+};
+
+Credentials requestCredentials() {
+    string username;
     std::cout << "Enter your username: ";
     std::cin >> username;
 
     string password;
     std::cout << "Enter your password: ";
     std::cin >> password;
-    string insertCmd = "INSERT INTO users (username, password) VALUES('"+ username + "', '" + password + "');";
-    PGresult *res = PQexec(conn, insertCmd.c_str());
+
+    return Credentials{username, password};
+}
+
+void insertUser(PGconn *conn) {
+    Credentials credentials = requestCredentials();
+    string insertQuery = "INSERT INTO users (username, password) VALUES('"+ credentials.username + "', '" + credentials.password + "');";
+
+ 
+    PGresult *res = PQexec(conn, insertQuery.c_str());
+    
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
         std::cout << "Insert into user table failed: " << PQresultErrorMessage(res) << '\n';
         exit(1);
+    } else {
+        std::cout << "User successfully registered into the database!\n";
     }
 }
+
+void getUser(PGconn *conn) {
+    Credentials credentials = requestCredentials();
+    string selectQuery = "SELECT * FROM users WHERE username = '"+ credentials.username +"';\n";
+    std::cout << selectQuery.c_str() << '\n';
+    PGresult *res = PQexec(conn, selectQuery.c_str());
+    std::cout<<"Working??\n";
+    
+    ExecStatusType status = PQresultStatus(res);
+    printf("Execution status: %d\n", status);
+
+    if (PQresultStatus(res) != PGRES_TUPLES_OK) {
+        std::cout << "Retrieval of user from users failed: " << PQresultErrorMessage(res) << " end\n";
+        exit(1);
+    } else {
+        printf("works\n");
+        for (int i = 0; i < PQnfields(res); i++) {
+            std::cout << PQfname(res, i) << '\n';
+        }
+        for (int i = 0; i < PQntuples(res); i++) {
+            for (int j = 0; j < PQnfields(res); j++) {
+                std::cout << PQgetvalue(res, i, j) << "   ";
+            }
+      std::cout << std::endl;
+    }
+    }
+
+}
+
 
 int main(int argc, char* argv[]){
     printf("Hello, from login_registerkd!\n");
@@ -50,10 +95,11 @@ int main(int argc, char* argv[]){
         switch(choice) {
         case 1: 
             printf("option 1: Login");
-            insertUser(conn);
+            getUser(conn);
             break;
         case 2: 
             printf("option 2: Register");
+            insertUser(conn);
             break;
         case 3: 
             printf("option 3: Update");
